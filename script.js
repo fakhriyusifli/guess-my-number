b6aa6105859f8fe625a706790d202d0b476de757
Fakhri Yusifli <fakhriyusifli@gmail.com>
2020-11-09 00:45:54 +0400

    manipulating style in win

--- script.js
+++ script.js
@@ -8,10 +8,18 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
   if (!guess) {
+    //when there is not any number between 1 and 20
+
     document.querySelector('.message').textContent = 'No guessed number';
   } else if (guess === secretNumber) {
+    //when the guessed number is equal to the secret number
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
     document.querySelector('.message').textContent = 'Correct Guess!';
   } else if (guess > secretNumber) {
+    //when the guessed number is greater than the secret number
+
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high...';
       score--;
@@ -21,6 +29,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secretNumber) {
+    //when the guessed number is less than the secret number
+
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low...';
       score--;
