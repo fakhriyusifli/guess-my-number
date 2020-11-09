5ea78178b0957efa3f6573301ca8aaa4e6086ac9
Fakhri Yusifli <fakhriyusifli@gmail.com>
2020-11-09 12:17:05 +0400

    developing again button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -13,7 +13,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No guessed number';
   } else if (guess === secretNumber) {
     //when the guessed number is equal to the secret number
-
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.message').textContent = 'Correct Guess!';
@@ -40,4 +40,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = 'grey';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').style.width = '15rem';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 });
